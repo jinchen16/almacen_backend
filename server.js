42220d46af28f11b39a81f81b984a5bd7cf68ece
Juan David Orejuela <juan.orejuela@hotmail.com>
2016-04-15 15:56:11 +0000

    Funciona Solicitar Prestamos y Aprobar

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var port = 8081;
+var port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -21,13 +21,13 @@ app.use(express.static(__dirname + "/app"));
 
 app.get('/create/:collection/:v1/:v2/:v3/:v4/:v5/:v6/:v7',service.create);
 app.get('/read/:collection',service.read);
-app.get('/update/:collection/:v1/:v2/:v3/:v4/:v5/:v6/:v7/:id',service.update);
 app.get('/delete/:collection/:param/:value',service.delete);
 
 //Métodos que se está utilizando actualmente
 app.get("/registrar/:collection/:v1/:v2/:v3/:v4/:v5/:v6", service.registrar);
 app.get("/login/:collection/:codigo/:contrasena", service.login);
 app.get('/read/:collection/:param/:value',service.readX);
+app.get('/update/:collection/:v1/:v2/:v3/:v4/:v5/:v6/:v7/:id',service.update);
 //app.get("/buscarUsuarios/:collection/:param/:value", service.buscarUsuarios);
 
 http.createServer(app).listen(port);
